c265aaf4d4159a987c77fb7c0969404f7d130ce0
iblamer <fabio_dlc96@hotmail.com>
2016-10-11 15:41:21 -0400

    Entidad medicina mejorada

--- Entidades/Medicinas.cs
+++ Entidades/Medicinas.cs
@@ -8,11 +8,16 @@ namespace Entidades
 {
     public class Medicinas
     {
+        public const double ITBIS = 0.18;
         [Key]
         public int idMed { set; get; }
         public string nombreMed { set; get; }
         public string descripcionMed { set; get; }
-        public double precioCompra { set; get; }
-        public double precioVenta { set; get; }
+        public float precioCompra { set; get; }
+        public float precioVenta { set; get; }
+        public DateTime FechaVencimiento { set; get; }
+        public float precioDetalle { set; get; }
+        public int cantidadExistencia { set; get; }
+
     }
 }
